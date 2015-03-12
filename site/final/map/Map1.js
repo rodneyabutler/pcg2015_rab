1026ccecd9cd002474874aa3d1624b1c1df93124
rodneyabutler <rodneyabutler@gmail.com>
2015-03-11 18:40:50 -0700

    Day two of JS

---
+++ site/final/map/Map1.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Rodney on 3/10/2015.
+ */
+
+<div class="container-fluid">
+	<div class="row-fluid">
+          <div class="span2 blue">How to make this background take the whole height of the screen/column</div>
+          <div class="span10 grey">Main text content here Main text content here Main text content here Main text
+              content here Main text content here
+          </div>
+	</div>
+</div>
+
+
+.offset2 {margin-left: 180px;}
+[class*="span"] {float: left;min-height: 1px;margin-left: 20px;}
+.row-fluid .offset2 {margin-left: 19.148936170212764%;}
+.row-fluid .offset2:first-child {margin-left: 17.02127659574468%;}
+.row-fluid {width: 98.678414097%;*zoom: 1;padding: 0 1.321585903%;}
+.row-fluid:before,.row-fluid:after {display: table;line-height: 0;content: "";}
+.row-fluid:after {clear: both;}
+.row-fluid [class*="span"] {display: block;float: left;width: 100%;/*min-height: 30px;*/margin-left:3.15186246418338%;
+*margin-left:2.074468085106383%;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;}
+.row-fluid [class*="span"]:first-child {margin-left: 0;}
+.row-fluid .controls-row [class*="span"] + [class*="span"] {margin-left: 2.127659574468085%;}
+.row-fluid .span12 {width: 100%;*width: 99.94680851063829%;}
+.row-fluid .span11 {width: 91.404011461318%;*width: 91.3562559694365%;}
+.row-fluid .span10 {width: 82.8080229226361%;*width: 82.7602674307546%;}
+.row-fluid .span9 {width: 74.2120343839542%;*width: 74.1642788920726%;}
+.row-fluid .span8 {width: 65.6160458452722%;*width: 65.5682903533906%;}
+.row-fluid .span7 {width: 57.0200573065903%;*width: 56.9723018147087%;}
+.row-fluid .span6 {width: 48.4240687679083%;*width: 48.3763132760267%;}
+.row-fluid .span5 {width: 39.8280802292264%;*width: 39.7803247373448%;}
+.row-fluid .span4 {width: 31.2320916905444%;*width: 31.1843361986628%;}
+.row-fluid .span3 {width: 22.6361031518625%;*width: 22.5883476599809%;}
+.row-fluid .span2 {width: 14.0401146131805%;*width: 13.992359121299%;}
+.row-fluid .span1 {width: 5.44412607449857%;*width: 5.396370582617%;}
